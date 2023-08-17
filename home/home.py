383403e9ec8ed1a47bf86ed47c8d1468fc100f01
davidgalfi <galfidavid314@gmail.com>
2023-08-17 23:13:41 +0200

    # Create the main home page and fix bugs
    Added the static_folder to the home Blueprint. Created the home.html. Created the admin_base.html, the user_base.html and the home_base.html files.

--- home/home.py
+++ home/home.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template
 
 # Creating the home blueprint
-home = Blueprint("home", __name__, template_folder="templates")
+home = Blueprint("home", __name__, template_folder="templates", static_folder="static")
 
 
 # Define the home route
