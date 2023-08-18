5bed02a1e7b9e97a7c94f8f6d3b9f6b2bcd04ee9
davidgalfi <galfidavid314@gmail.com>
2023-08-18 10:36:15 +0200

    # Create login and register pages and their routes

--- home/home.py
+++ home/home.py
@@ -9,3 +9,12 @@ home = Blueprint("home", __name__, template_folder="templates", static_folder="s
 @home.route("/")
 def main_home():
     return render_template("home.html")
+
+
+@home.route("/login")
+def login():
+    return render_template("login.html")
+
+@home.route("/register")
+def register():
+    return render_template("register.html")
