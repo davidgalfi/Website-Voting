c90a04230e369b9575807f6b9fb4c0c4822509ca
davidgalfi <galfidavid314@gmail.com>
2023-08-18 17:06:41 +0200

    # Create the admin_home and the admin_base html files.

--- admin/admin.py
+++ admin/admin.py
@@ -1,11 +1,11 @@
 from flask import Blueprint, render_template
 
 # Creating the admin blueprint
-admin = Blueprint("admin", __name__, template_folder="templates")
+admin = Blueprint("admin", __name__, template_folder="templates", static_folder="static")
 
 
 # Define the home route
-@admin.route("/main_home")
+@admin.route("/admin_home")
 @admin.route("/")
 def admin_home():
     return render_template("admin_home.html")
